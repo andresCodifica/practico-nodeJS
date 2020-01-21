28c99fa02a93ea2a421bd4bd0868b1c76eba95ad
jhairbury029 <janska029@gmail.com>
2020-01-20 22:25:54 -0500

    rutas para usuario getAll(list), getOne() add()upsert dummy

--- api/components/user/network.js
+++ api/components/user/network.js
@@ -1,14 +1,42 @@
 const express = require('express');
 
 const response = require('../../../network/response')
-const Controller = require('./controller')
+const Controller = require('./index')
 
 const router = express.Router();
 
 router.get('/',function (req,res){
-    const lista = Controller.list()
-    //res.send('Todo funciona')
-    response.success(req,res,lista,200)
+    Controller.list()
+        .then((lista)=>{
+            //res.send('Todo funciona')
+            response.success(req,res,lista,200)
+        }).catch((err)=>{
+            response.error(req,res,err.message,500)
+        })
+    
+})
+
+router.get('/:id',function (req,res){
+    Controller.get(req.params.id)
+        .then((user)=>{
+        //res.send('Todo funciona')
+        response.success(req,res,user,200)
+        }).catch((err)=>{
+            response.error(req,res,err.message,500)
+        })
+    
+})
+router.post('/:id/:name',function (req,res){
+    let data = {
+        id:req.params.id,
+        name:req.params.name
+    }
+    Controller.upsert(data)
+        .then((doc)=>{
+            response.success(req,res,doc,200) 
+        }).catch((err)=>{
+            response.error(req,res,err.message,500)
+        })
 })
 
 module.exports = router;
--- store/dummy.js
+++ store/dummy.js
@@ -1,24 +1,25 @@
 const db = {
     'user': [
-        {id: 1, name: 'Carlos'},
+        {id: '1', name: 'Carlos'},
     ]
 };
 
-function list(tabla){
+async function list(tabla){
  return db[tabla];
 }
 
-function get(tabla,id){
-    let col = list(tabla);
+async function get(tabla, id){
+    let col = await list(tabla);
     return col.filter(item => item.id == id) [0] || null;
 }
 
-function upsert(tabla, data){
-    db[collection].push(data)
+async function upsert(tabla, data){
+    db[tabla].push(data)
+    return data
 }
 
-function remove(tabla,id){
-    return true;
+async function remove(tabla,id){
+    return new true;
 }
 
 module.exports = {
